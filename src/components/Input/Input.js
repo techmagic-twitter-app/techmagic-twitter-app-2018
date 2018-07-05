a703f8ed3808f058397ff1bd1a6d82c11dc5dabc
Bogdan-Lishchynskiy <lishchynskiy91@gmail.com>
2018-07-06 01:57:39 +0300

    add permision user & added name and avatar to user post

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -23,10 +23,15 @@ setInputVal=(event)=>{
 }
 
 sendMsg=()=>{
+    const { user } = this.props;
+    const { post } = this.state;
     this.props.sendMessage({
-        post:this.state.post,
-        likes:0,
-        date:Date.now()
+        post:  post,
+        likes: 0,
+        date: Date.now(),
+        userId: user.uid,
+        userName: user.displayName,
+        userPhoto: user.photoURL
     })
     this.setState({
         post:''
@@ -36,19 +41,92 @@ sendMsg=()=>{
 
 
 render() {
-return (
-    <div className="Input">
-        <input type="text" onChange={this.setInputVal} value={this.state.post} />
-        <button onClick={this.sendMsg}>Send</button>
-    </div>
-    )
-}
+    const { user } = this.props;
+    return (
+        user?
+        <div className="Input">
+            <input type="text" onChange={this.setInputVal} value={this.state.post} />
+            <button onClick={this.sendMsg}>Send</button>
+        </div>: null
+)}
 
 
 }
-
+const mapStateToProps = state =>({
+    user: state.user
+})
+  
 const mapDispatchToProps = dispatch => bindActionCreators({
     sendMessage
 },dispatch)
 
-export default connect(null,mapDispatchToProps)(Input);
+export default connect(mapStateToProps, mapDispatchToProps)(Input);
+
+
+// import React, { Component } from 'react';
+
+// // Actions
+// import {sendMessage} from '../../actions/posts';
+// import {connect} from 'react-redux';
+// import {bindActionCreators} from 'redux';
+
+// // Styles
+// import './Input.css'
+
+//  class Input extends Component {
+
+// state = { 
+//     post:'',
+// }
+
+
+
+// setInputVal=(event)=>{
+//     this.setState({
+//         post:event.target.value
+//     })
+// }
+
+// sendMsg=()=>{
+//     this.props.sendMessage({
+//         post:this.state.post,
+//         likes:0,
+//         date:Date.now()
+//     })
+//     this.setState({
+//         post:''
+//     })
+// }
+
+
+
+// render() {
+//     const { user, post } = this.props;
+//     return (
+//         user?
+//         <div className="Input">
+//             <input type="text" onChange={this.setInputVal} value={post} />
+//             <button onClick={this.sendMsg}>Send</button>
+//         </div>: null
+// )}
+
+// // return (
+// //     <div className="Input">
+// //         <input type="text" onChange={this.setInputVal} value={this.state.post} />
+// //         <button onClick={this.sendMsg}>Send</button>
+// //     </div>
+// //     )
+// // }
+
+
+// }
+// const mapStateToProps = state =>({
+//     user: state.user
+// })
+
+
+// const mapDispatchToProps = dispatch => bindActionCreators({
+//     sendMessage
+// },dispatch)
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Input);
--- src/components/PostsList/Post/Post.js
+++ src/components/PostsList/Post/Post.js
@@ -8,15 +8,19 @@ class Post extends Component {
   dateForma = (data )=> moment(data).format("MMM Do YY");
 
   render() {
+ 
     const { currentPost } = this.props
       return (
       <li className="Post">
+      <img src={currentPost.userPhoto} alt="userPhoto" width="50" height="50"/>
+        <h4>{currentPost.userName}</h4>
         <h3>{currentPost.post}</h3>
         <p>date:{this.dateForma(currentPost.date)}</p>
         <p><i className="fas fa-heart"></i>{currentPost.likes}</p>
       </li>
     )
   }
+    
 }
+export default Post;
 
-export default Post
