2680e6cf9c75857ca7db05c537ad86d873667ccb
Bogdan-Lishchynskiy <lishchynskiy91@gmail.com>
2018-07-06 01:01:14 +0300

    fix date format

--- src/components/PostsList/Post/Post.js
+++ src/components/PostsList/Post/Post.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
 
-// styles
-import './Post.css'
+import './Post.css';
 
-export default ({currentPost}) => {
-  return (
-    <li className="Post">
-      <h3>{currentPost.post}</h3>
-      <p>date:{currentPost.date}</p>
-      <p><i className="fas fa-heart"></i>{currentPost.likes}</p>
-    </li>
-  )
+class Post extends Component {
+
+  dateForma = (data )=> moment(data).format("MMM Do YY");
+
+  render() {
+    const { currentPost } = this.props
+      return (
+      <li className="Post">
+        <h3>{currentPost.post}</h3>
+        <p>date:{this.dateForma(currentPost.date)}</p>
+        <p><i className="fas fa-heart"></i>{currentPost.likes}</p>
+      </li>
+    )
+  }
 }
+
+export default Post
