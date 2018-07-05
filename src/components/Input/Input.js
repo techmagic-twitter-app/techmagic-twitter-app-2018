279a16814f722bf1169d7633c2521302357c362d
Bogdan-Lishchynskiy <lishchynskiy91@gmail.com>
2018-07-06 02:02:22 +0300

    cleared comment code

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -15,7 +15,6 @@ state = {
 }
 
 
-
 setInputVal=(event)=>{
     this.setState({
         post:event.target.value
@@ -61,72 +60,3 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 },dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Input);
-
-
-// import React, { Component } from 'react';
-
-// // Actions
-// import {sendMessage} from '../../actions/posts';
-// import {connect} from 'react-redux';
-// import {bindActionCreators} from 'redux';
-
-// // Styles
-// import './Input.css'
-
-//  class Input extends Component {
-
-// state = { 
-//     post:'',
-// }
-
-
-
-// setInputVal=(event)=>{
-//     this.setState({
-//         post:event.target.value
-//     })
-// }
-
-// sendMsg=()=>{
-//     this.props.sendMessage({
-//         post:this.state.post,
-//         likes:0,
-//         date:Date.now()
-//     })
-//     this.setState({
-//         post:''
-//     })
-// }
-
-
-
-// render() {
-//     const { user, post } = this.props;
-//     return (
-//         user?
-//         <div className="Input">
-//             <input type="text" onChange={this.setInputVal} value={post} />
-//             <button onClick={this.sendMsg}>Send</button>
-//         </div>: null
-// )}
-
-// // return (
-// //     <div className="Input">
-// //         <input type="text" onChange={this.setInputVal} value={this.state.post} />
-// //         <button onClick={this.sendMsg}>Send</button>
-// //     </div>
-// //     )
-// // }
-
-
-// }
-// const mapStateToProps = state =>({
-//     user: state.user
-// })
-
-
-// const mapDispatchToProps = dispatch => bindActionCreators({
-//     sendMessage
-// },dispatch)
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Input);
