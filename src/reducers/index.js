1a3ffe8a8512ca690cee0d2dc6c4c15792e88f7e
Marko Nazarii <physicist1m@github.com>
2018-07-06 15:45:03 +0300

    connect users to posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import userReducer from './user';
 import postReducers from './posts';
 import usersReducer from './users';
+import uidReducer from './uid';
 
 
 
 export default combineReducers({
     user:userReducer,
     posts:postReducers,
-    users:usersReducer
+    users:usersReducer,
+    uid:uidReducer
 });
