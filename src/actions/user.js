16a94e795787a884af06e4aa5b78282b710fcf7a
andrew <andrew.makar77@gmail.com>
2018-07-09 23:37:45 +0300

    fix user validation

--- src/actions/user.js
+++ src/actions/user.js
@@ -4,29 +4,24 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => dispatch => {
   authRef.onAuthStateChanged(user => {
     if (user) {
-      var sameUsers = 0;
-      let currentUserUid = user.uid;
-      usersRef.once('value').then(snapshot => {
-        Object.values(snapshot.val())
-          .forEach((item, index, arr) => {
-          if(currentUserUid === item.uid) {
-            sameUsers++;
-          } else if (sameUsers===0 && index===arr.length-1){
-            //check if such user exists
-            //and add new user into db
-            usersRef.push().set({
-              username: user.displayName,
-              uid: user.uid,
-              photo: user.photoURL
-            });
-          }
-        })
-      });
+    usersRef.orderByChild("uid")
+    .equalTo(user.uid)
+    .once("value")
+    .then(snapshot => {
+      if (!snapshot.val()) {
+        usersRef.push().set({
+          username: user.displayName,
+          uid: user.uid,
+          photo: user.photoURL
+        });
+      }
+  });
       dispatch({
         type: FETCH_USER,
         payload: user
       });
-    } else {
+    }
+    else {
       dispatch({
         type: FETCH_USER,
         payload: ''
@@ -34,7 +29,6 @@ export const fetchUser = () => dispatch => {
     }
   });
 };
-
 
 
 
