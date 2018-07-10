026b3e7d7f38a3739320061201509c04a2a331e2
Marko Nazarii <physicist1m@github.com>
2018-07-10 16:52:36 +0300

    remove redux devtools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './App';
@@ -9,10 +9,7 @@ import  './index.css'
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducers,
-  compose(applyMiddleware(reduxThunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && 
-  window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(reduxThunk)
 );
 
 ReactDOM.render(
